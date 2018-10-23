6b4b1b220985af3a29fb174b35c1bec418bac3a4
Waqas Karim <w.karim@belonginteractive.com>
2018-10-23 14:12:04 +0400

    latest code updated fixed responsive till ipad

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-    $('select').niceSelect();
+    //$('select').niceSelect();
     $('.datepicker').flatpickr({
         dateFormat: "Y-m-d",
         disable : ['2018-10-23']
@@ -9,7 +9,7 @@ $(function(){
     $('.testimonial').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        infinite: false,
+        infinite: true,
         arrows: true,
         prevArrow: $('.slider-controls .arrow.left'),
         nextArrow: $('.slider-controls .arrow.right')
@@ -18,10 +18,19 @@ $(function(){
     $('.box-slider').slick({
         slidesToShow: 2,
         slidesToScroll: 1,
-        infinite: false,
+        infinite: true,
         arrows: true,
         prevArrow: $('.box-slider-controls .arrow.left'),
         nextArrow: $('.box-slider-controls .arrow.right')
+    }); 
+
+
+    $('.thumbnail-slider').slick({        
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: false,                
+        useCSS : false
     });
 
 
@@ -35,4 +44,22 @@ $(function(){
         $('.main-navigation').toggleClass('close');
       });
 
-})
+
+      /* Accordion */
+      $('.accordion').beefup({
+        openSingle: true,
+      });
+
+
+});
+
+function doResize() {
+    // FONT SIZE
+    var ww = $('body').width();
+    var maxW = 1920;
+    ww = Math.min(ww, maxW);
+    var fw = ww*(10/maxW);
+    var fpc = fw*100/16;
+    var fpc = Math.round(fpc*100)/100;
+    $('html').css('font-size',fpc+'%');
+}
